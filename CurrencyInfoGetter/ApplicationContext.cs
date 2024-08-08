015c8a30ce793a3d70f6a540e4b3e57f14276eef
Dubster <shevc9@mail.ru>
2024-08-08 22:59:33 +0300

    Database connection added

---
+++ CurrencyInfoGetter/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using CurrencyInfoGetter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyInfoGetter
+{
+	public class ApplicationContext: DbContext
+	{
+		public DbSet<Rate> Currencies => Set<Rate>();
+
+		private readonly string _connectionString;
+		public ApplicationContext(IConfiguration config)
+		{
+			_connectionString = $"Server={config["DbServerAddress"]};User Id={config["DbUserId"]};" +
+				$"Password={config["DbPassword"]};Port={config["DbPort"]};Database={config["DbName"]};";
+			Database.EnsureCreated();
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(_connectionString);
+		}
+	}
+}
--- CurrencyInfoGetter/Program.cs
+++ CurrencyInfoGetter/Program.cs
@@ -6,9 +6,9 @@ namespace CurrencyInfoGetter
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
-
+			var config = builder.Configuration;
 			// Add services to the container.
-
+			builder.Services.AddDbContext<ApplicationContext>();
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
